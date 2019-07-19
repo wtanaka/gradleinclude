b219e006d0258932817f8b46b8677be5626926ad
wtanaka.com <wtanaka@users.noreply.github.com>
2019-07-19 14:43:05 -0700

    Checkstyle

--- src/main/java/com/wtanaka/gradleinclude/appenginestandard/AppEngineStandardServlet.java
+++ src/main/java/com/wtanaka/gradleinclude/appenginestandard/AppEngineStandardServlet.java
@@ -14,18 +14,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License. See accompanying LICENSE file.
  */
+
 package com.wtanaka.gradleinclude.appenginestandard;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
+/**
+ * Example App Engine servlet code.
+ */
 public class AppEngineStandardServlet extends HttpServlet {
     private static final long serialVersionUID = 6449522932459211583L;
 
+    /**
+     * Respond to GET.
+     * @param request request object
+     * @param response response object
+     * @throws ServletException Servlet-layer exception
+     * @throws IOException Network exception
+     */
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Set response content type
@@ -33,8 +44,8 @@ public class AppEngineStandardServlet extends HttpServlet {
 
         // Actual logic goes here.
         PrintWriter out = response.getWriter();
-        out.println("<h1>hello from" +
-                " <a href=\"https://github.com/wtanaka/gradleinclude\">" +
-                "gradleinclude</a></h1>");
+        out.println("<h1>hello from"
+                + " <a href=\"https://github.com/wtanaka/gradleinclude\">"
+                + "gradleinclude</a></h1>");
     }
 }
