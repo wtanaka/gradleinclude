e385a4fc4df1b6f1b2fb2852d34368715456d47a
wtanaka.com <wtanaka@users.noreply.github.com>
2019-09-27 14:16:28 -0700

    Example "Hello World" in Kotlin

--- src/main/kotlin/com/wtanaka/gradleinclude/Example.kt
+++ src/main/kotlin/com/wtanaka/gradleinclude/Example.kt
@@ -4,3 +4,10 @@ import java.util.Date
  * This is an example function.
  */
 fun example(): String = "Hello" + Date()
+
+/**
+ * Command line entrypoint.
+ */
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
